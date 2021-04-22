fb48ec63293b177d412bffe6d7ade68ae1baf82d
Ayxan Axundov <axundovayxan9@gmail.com>
2021-04-22 13:30:00 +0400

    Add project files.

---
+++ CinemaProject/logIn.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaProject
+{
+    //class logIn
+    //{
+    //    public int Id { get; set; }
+    //    private static int _StaticId { get; set; } = 0;
+        
+    //    public logIn()
+    //    {
+    //        Id = ++_StaticId;
+    //    }
+
+    //    public string Username{ get; set; }
+    //    public string Email { get; set; }
+    //    public string Password{ get; set; }
+
+
+
+    //}
+}
